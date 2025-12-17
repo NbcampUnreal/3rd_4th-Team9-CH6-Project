b318fd0d3aca4c5b5f028379d5ff00593b62aa9a
DiooN <dioon0000@naver.com>
2025-12-17 17:40:40 +0900

    Feat : AI Parent Class 변경 ( Character -> RSEnemyBaseCharacter )
    - AI의 EnemyBaseCharacter C++ 코드 작성
    - 기존 AI의 부모 클래스 변경
    - GameplayAbilitySystem Component 추가

---
+++ Source/RemnantSoul/Private/AI/RSEnemyBaseCharacter.cpp
@@ -0,0 +1,49 @@
+﻿#include "AI/RSEnemyBaseCharacter.h"
+#include "AbilitySystemComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+ARSEnemyBaseCharacter::ARSEnemyBaseCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	/* GAS */
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystem"));
+	AbilitySystemComponent->SetIsReplicated(false);
+	
+	//AttributeSet = CreateDefaultSubobject<URSAttributeSet>(TEXT("AttributeSet"));
+
+	/* Set Size for collision capsule*/
+	GetCapsuleComponent()->InitCapsuleSize(35.0f, 90.0f);
+	
+	/* Don't rotate when the controller rotates */
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	/* Configure Character Movement */
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 360.0f, 0.0f);
+
+	GetCharacterMovement()->JumpZVelocity = 500.f;
+	GetCharacterMovement()->AirControl = 0.35f;
+	GetCharacterMovement()->MaxWalkSpeed = 500.0f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+	GetCharacterMovement()->BrakingDecelerationFalling = 1500.f;
+}
+
+void ARSEnemyBaseCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+
+	if (AbilitySystemComponent)
+	{
+		AbilitySystemComponent->InitAbilityActorInfo(this, this);
+	}
+}
+
+UAbilitySystemComponent* ARSEnemyBaseCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
---
+++ Source/RemnantSoul/Public/AI/RSEnemyBaseCharacter.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
+#include "RSEnemyBaseCharacter.generated.h"
+
+class UAbilitySystemComponent;
+
+UCLASS()
+class REMNANTSOUL_API ARSEnemyBaseCharacter : public ACharacter, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	ARSEnemyBaseCharacter();
+	
+	/* GAS */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GAS")
+	UAbilitySystemComponent* AbilitySystemComponent;
+	
+protected:
+	virtual void PossessedBy(AController* NewController) override;
+
+public:
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+};
